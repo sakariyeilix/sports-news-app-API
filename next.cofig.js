1f37d0bc9d12e31d7c0af435576489ea0cb9a433
sakariyeilix <sakariyeilix@gmail.com>
2024-07-20 22:01:13 +0300

    update

--- next.cofig.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslimt:{
-        ignoreDuringBuilds: true,
-    },
-
-}
-module.exports = nextConfig
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -19,30 +19,28 @@ function Layout() {
   }, []);
   
   return (
-    <>
-      <div className="grid gap-2 lg:grid-cols-4">
-        {value.map((a, key) => (
-          <div className="w-full rounded-lg shadow-md lg:max-w-sm" key={key}>
-            <img
-              className="object-cover w-full h-48"
-              src={a.urlToImage || "fallback-image-url"}
-              alt="image"
-            />
-            <div className="p-4">
-              <h4 className="text-xl font-semibold text-blue-600">
-                {a.title}
-              </h4>
-              <p className="mb-2 leading-normal">
-                {a.content}
-              </p>
-              <a href={a.url} className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-                Read more
-              </a>
-            </div>
+    <div className="grid gap-2 lg:grid-cols-4">
+      {value.map((a, index) => (
+        <div className="w-full rounded-lg shadow-md lg:max-w-sm" key={index}>
+          <img
+            className="object-cover w-full h-48"
+            src={a.urlToImage || "fallback-image-url"}
+            alt="image"
+          />
+          <div className="p-4">
+            <h4 className="text-xl font-semibold text-blue-600">
+              {a.title}
+            </h4>
+            <p className="mb-2 leading-normal">
+              {a.content || "No content available."}
+            </p>
+            <a href={a.url} className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow" target="_blank" rel="noopener noreferrer">
+              Read more
+            </a>
           </div>
-        ))}
-      </div>
-    </>
+        </div>
+      ))}
+    </div>
   );
 }
 
